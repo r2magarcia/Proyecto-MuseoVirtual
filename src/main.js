3306efa84c10282867b42e875de8139645d3cc92
r2magarcia <ma.garcia2r@gmail.com>
2020-11-24 16:13:49 -0500

    carga de algunos modelos con texturas

--- src/main.js
+++ src/main.js
@@ -8,7 +8,10 @@ var scene = null,
     clock = null,
     ambientLight=null,
     interval = 5000,
-    container;
+    container,
+    camHolder;
+var VspotLight=[];
+var VHelpers=[];
 
 var sound1 = null,
     countPoints = null,
@@ -47,6 +50,12 @@ var posZ = 1;
 var player = null;
 var playerCreated = false;
 
+var speedTrans = 5;
+var speedRot = THREE.Math.degToRad(45);
+
+var clock = new THREE.Clock();
+var delta = 0;
+
 var
     hemisphereLight;
 // ----------------------------
@@ -56,6 +65,7 @@ function start() {
     window.onresize = onWindowResize;
     initScene();
     animate();
+    // pin.traverse(function(child){child.castShadow = true;});
 }
 
 function onWindowResize() {
@@ -66,30 +76,35 @@ function onWindowResize() {
 
 function initScene() {
     initBasicElements(); // Scene, Camera and Render
-    createMultiplyPick();
-    initSound();         // To generate 3D Audio
     createLight();       // Create light
     initWorld();
-    createPlayerMove();
+    // createPlayerMove();
     // initGUI();
 }
 
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    sound1.update(camera);
-    movePlayer();
-    // collisionAnimate();
+    delta = clock.getDelta();
+    // movePlayer();
+    collisionAnimate();
+}
+
+function createSculptures(){
+    // createPickUp(1,1,'Drop');
+    createPickUp(5,5,'copa');
+    createPickUp(-5,-5,'alcarraza');
+    
 
 }
 
 function initBasicElements() {
     container = document.getElementById( 'canvas' );
-    console.log(container);
+    // console.log(container);
     document.body.appendChild( container );
 
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, (window.innerWidth-100) / (window.innerHeight-100), 0.1, 1000);
+    camera = new THREE.PerspectiveCamera(60, (window.innerWidth-100) / (window.innerHeight-100), 1, 1000);
     renderer = new THREE.WebGLRenderer();
     // renderer.setSize( parseInt(container.style.width), parseInt(container.style.height) );
     renderer.setSize(window.innerWidth-100,window.innerHeight-100);
@@ -100,17 +115,18 @@ function initBasicElements() {
     scene.fog = new THREE.Fog(0xffffff, 0, 750);
 
 
-    camera.position.x = 3;
-    camera.position.y = 0.5;
-    camera.position.z = 1;
+    // camera.position.x = 0;
+    // camera.position.y = 1.8;
+    // camera.position.z = 0;
+
+    camHolder = new THREE.Group();
+    camHolder.add(camera);
+    // camHolder.add(MovingCube);
+    camHolder.position.set(5,1.7,5);
+    createPlayerMove();
+    scene.add(camHolder);
 }
 
-function initSound() {
-    sound1 = new Sound(["./songs/Tokyo_Drift.mp3"], 500, scene, {
-        debug: true,
-        position: { x: camera.position.x, y: camera.position.y + 10, z: camera.position.z }
-    });
-}
 
 function createModel(generalPath, pathMtl, pathObj, whatTodraw) {
     var mtlLoader = new THREE.MTLLoader();
@@ -141,7 +157,7 @@ function createModel(generalPath, pathMtl, pathObj, whatTodraw) {
                     object.scale.set(0.005, 0.005, 0.005);
                     object.position.x = MovingCube.position.x;
                     object.position.y = MovingCube.position.y;
-                    object.position.z = MovingCube.position.z+2;
+                    object.position.z = MovingCube.position.z;
                     object.rotation.y = Math.PI / 2 + Math.PI / 2;
                     player = object;
                     
@@ -195,7 +211,8 @@ function createLight() {
 
 function initWorld() {
 
-    createModel('./modelos/Museo/', 'MuseoProyecto.mtl', 'MuseoProyecto.obj', 'world');
+    // createModel('./modelos/Museo/', 'MuseoProyecto.mtl', 'MuseoProyecto.obj', 'world');
+    createModel('./modelos/MuseoArreglado/', 'MuseoArreglado.mtl', 'MuseoArreglado.obj', 'world');
     // createModel('./modelos/Car/', 'Cartoon_Lowpoly_Car.mtl', 'Cartoon_Lowpoly_Car.obj', 'player');
 
     // floor
@@ -205,7 +222,9 @@ function initWorld() {
 
     var floorMaterial = new THREE.MeshBasicMaterial({ color: 0x383535 });
     var floor = new THREE.Mesh(floorGeometry, floorMaterial);
-    scene.add(floor);
+    // scene.add(floor);
+
+    createSculptures();
 }
 
 function initGUI() {
@@ -232,71 +251,44 @@ function initGUI() {
 
   gui.close();
 }
-// ----------------------------------
-// Función Para mover al jugador:
-// ----------------------------------
-function movePlayer() {
-    var delta = clock.getDelta(); // seconds.
-    var moveDistance = 3 * delta; // 200 pixels per second
-    if (figuresGeo.length <= 0)
-        movTra = false; // ("No hay player")
-    else
-        movTra = true; // ("Hay player")
-
-    if (input.right == 1) {
-        //camera.rotation.y -= rotSpd;
-        MovingCube.rotation.y -= rotSpd;
-        // player.rotation.y -= rotSpd;
-        // modelPlay.rotation.y  -= rotSpd;
-    }
-    if (input.left == 1) {
-        //camera.rotation.y += rotSpd;
-        MovingCube.rotation.y += rotSpd;
-        // player.rotation.y += rotSpd;
-        // modelPlay.rotation.y  += rotSpd;
-    }
-
-    if (input.up == 1) {
-        MovingCube.translateZ(-moveDistance);
-        MovingCube.translateZ(-moveDistance);
-    }
-    if (input.down == 1) {
-        MovingCube.translateZ(moveDistance);
-    }
-
-    if (playerCreated){ 
-        player.position.set(MovingCube.position.x, MovingCube.position.y, MovingCube.position.z)
-        player.translateX(0.6);
-    }
-    var relativeCameraOffset = new THREE.Vector3(0, 1.5, 3);
-
-    var cameraOffset = relativeCameraOffset.applyMatrix4(MovingCube.matrixWorld);
-
-    camera.position.x = cameraOffset.x;
-    camera.position.y = cameraOffset.y;
-    camera.position.z = cameraOffset.z;
-    camera.lookAt(MovingCube.position);
-}
-
-window.addEventListener('keydown', function(e) {
-    switch (e.keyCode) {
-        case 68:
-            input.right = 1;
-            break;
-        case 65:
-            input.left = 1;
-            break;
-        case 87:
-            input.up = 1;
-            break;
-        case 83:
-            input.down = 1;
-            break;
-        case 27:
-            document.getElementById("blocker").style.display = 'block';
-            break;
-    }
-});
+document.addEventListener('keydown', function(evt) {
+    // console.log(evt);
+    
+    if (evt.keyCode === 87) {
+      camHolder.translateZ(-speedTrans * delta);
+      MovingCube.translateZ( -speedTrans * delta);
+      
+    } // w fast vorward
+    if (evt.keyCode === 83) {
+      camHolder.translateZ( speedTrans * delta);
+      MovingCube.translateZ( speedTrans * delta);
+    } 
+  
+    if (evt.keyCode === 65) {
+      camHolder.rotateY(speedRot * delta);
+      MovingCube.rotateY(speedRot * delta);
+    } // l turn to the left
+    if (evt.keyCode === 68) {
+      camHolder.rotateY(-speedRot * delta);
+      MovingCube.rotateY(-speedRot * delta);
+    } // r turn to the right
+  
+    if (evt.keyCode === 84) {
+      camHolder.translateY(speedTrans * delta);
+      MovingCube.translateY(speedTrans * delta);
+    } // t upstretch
+    if (evt.keyCode === 66) {
+      camHolder.translateY(-speedTrans * delta);
+      MovingCube.translateY(-speedTrans * delta);
+    } // b bend down
+    if (evt.keyCode === 38) {
+      camera.rotation.x += speedRot * delta;
+    } // up arrow, looking higher
+    if (evt.keyCode === 40) {
+      camera.rotation.x += -speedRot * delta;
+    } // down arrow, looking deeper
+  
+  });
 
 
 window.addEventListener('keyup', function(e) {
@@ -334,27 +326,18 @@ function showInfoCreator() {
 
 }
 
-function createMultiplyPick() {
-  var xPos = 0
-  var yPos = 0
-
-  for(k=0;k<10;k++){
-    //   createPickUp(posiblePos[0+xPos],posiblePos[1+yPos]);
-      xPos +=2;
-      yPos +=2;
-  }
-}
 
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-function createPickUp(xPos,zPos) {
+function createPickUp(xPos,zPos,name) {
   // create a geometry
-  const geometry = new THREE.BoxBufferGeometry( 0.3, 0.3, 0.3 );
+  yPos=0.6;
+  const geometry = new THREE.BoxBufferGeometry( 2, 3, 2 );
   
   //  Create texture 
-  const material = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe:false, transparent: true, opacity: 0.0 });
+  const material = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe:true, transparent: true, opacity: 1 });
 
   // create a Mesh
   mesh[pickupNum] = new THREE.Mesh( geometry, material );
@@ -365,38 +348,66 @@ function createPickUp(xPos,zPos) {
   mesh[pickupNum].id   = "modelToPick"+randomIdentify;
   cont++;
 
-  mesh[pickupNum].position.x = (camera.position.x-2)+xPos;
-  mesh[pickupNum].position.y = camera.position.y;
-  mesh[pickupNum].position.z = camera.position.z-zPos;
+  mesh[pickupNum].position.x = xPos;
+  mesh[pickupNum].position.y = yPos;
+  mesh[pickupNum].position.z = zPos;
+  var spotLight = new THREE.SpotLight( 0xffffff );
+    spotLight.position.set( xPos, 4, zPos );
+
+    spotLight.castShadow = true;
+    spotLight.intensity=1;
+    spotLight.distance=5;
+    spotLight.angle=0.5;
+    spotLight.prenumbra=1;
+    spotLight.decay=1;
+    spotLight.focus=0.5;
+
+    spotLight.shadow.mapSize.width = 1024;
+    spotLight.shadow.mapSize.height = 1024;
+
+    spotLight.shadow.camera.near = 500;
+    spotLight.shadow.camera.far = 4000;
+    spotLight.shadow.camera.fov = 30;
+    spotLight.target=mesh[pickupNum];
+
+    VspotLight.push(spotLight);
+    scene.add( VspotLight[VspotLight.length-1] );
+    var spotLightHelper = new THREE.SpotLightHelper( VspotLight[VspotLight.length-1] );
+    VHelpers.push(spotLightHelper);
+    // scene.add( VHelpers[VHelpers.length-1] );
 
   // add the mesh to the scene collisions object
   collectibleMeshList.push(mesh[pickupNum]);
   scene.add(mesh[pickupNum]);
-  makeDrop(pickupNum, xPos, zPos);
+  loadSculpture(pickupNum, xPos, zPos, 'Pedestal');
+  loadSculpture(pickupNum, xPos, zPos, name)
+  console.log(collectibleMeshList);
+  console.log(cont);
   
 
   pickupNum++;
   
 }
 
-function makeDrop(pick, xPos, zPos){
+function loadSculpture(pick, xPos, zPos, name){
   var mtlLoader = new THREE.MTLLoader();
-    mtlLoader.setTexturePath("./modelos/Drop/");
-    mtlLoader.setPath("./modelos/Drop/");
-    mtlLoader.load("Drop.mtl", function (materials) {
+    mtlLoader.setTexturePath(`./modelos/${name}/`);
+    mtlLoader.setPath(`./modelos/${name}/`);
+    mtlLoader.load(`${name}.mtl`, function (materials) {
       
       materials.preload();
       var objLoader = new THREE.OBJLoader();
       objLoader.setMaterials(materials);
-      objLoader.setPath("./modelos/Drop/");
-      objLoader.load("Drop.obj", function (object) {
-      object.scale.set(0.2, 0.2, 0.2);
-      object.position.x = (camera.position.x-2)+xPos;
-      object.position.y = 0.6;
-      object.position.z = camera.position.z-zPos-3;
+      objLoader.setPath(`./modelos/${name}/`);
+      objLoader.load(`${name}.obj`, function (object) {
+      name=='Pedestal'?object.scale.set(0.0115, 0.0115, 0.0115):object.scale.set(1,1,1);
+      object.position.x = xPos;
+      object.position.y = name=='Pedestal'?0.2: 1.5;
+      object.position.z = zPos;
+      object.castShadow=true;
       modelpick[pick]=object;
       scene.add(object);
-      object.name = 'drop'+pick;
+      object.name = `${name}`+pick;
 
         });
 
@@ -406,81 +417,44 @@ function makeDrop(pick, xPos, zPos){
 // Funciones llamadas desde el index:
 // ----------------------------------
 function createPlayerMove() {
-    var cubeGeometry = new THREE.CubeGeometry(1, 1.6, 2.1, 1, 1, 1);
-    var wireMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: false, transparent: true, opacity: 0.0 });
+    var cubeGeometry = new THREE.CubeGeometry(2,2,2);
+    var wireMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true});
     MovingCube = new THREE.Mesh(cubeGeometry, wireMaterial);
-    MovingCube.position.set(camera.position.x, camera.position.y, camera.position.z);
+    MovingCube.position.set(5,1.7,5);
     scene.add(MovingCube);
 }
 
-// function collisionAnimate() {
+function collisionAnimate() {
 
-//     var originPoint = MovingCube.position.clone();
+    var originPoint = MovingCube.position.clone();
 
-//     for (var vertexIndex = 0; vertexIndex < MovingCube.geometry.vertices.length; vertexIndex++) {
-//         var localVertex = MovingCube.geometry.vertices[vertexIndex].clone();
-//         var globalVertex = localVertex.applyMatrix4(MovingCube.matrix);
-//         var directionVector = globalVertex.sub(MovingCube.position);
+    for (var vertexIndex = 0; vertexIndex < MovingCube.geometry.vertices.length; vertexIndex++) {
+        var localVertex = MovingCube.geometry.vertices[vertexIndex].clone();
+        var globalVertex = localVertex.applyMatrix4(MovingCube.matrix);
+        var directionVector = globalVertex.sub(MovingCube.position);
 
-//         var ray = new THREE.Raycaster(originPoint, directionVector.clone().normalize());
-//         var collisionResults = ray.intersectObjects(collidableMeshList);
-//         var collisionRCollect = ray.intersectObjects(collectibleMeshList);
+        var ray = new THREE.Raycaster(originPoint, directionVector.clone().normalize());
+        var collisionResults = ray.intersectObjects(collidableMeshList);
+        var collisionRCollect = ray.intersectObjects(collectibleMeshList);
 
-//         if (collisionRCollect.length > 0 && collisionRCollect[0].distance < directionVector.length()) {
-//             document.getElementById("points").innerHTML = points;
-//             var toErase = scene.getObjectByName('drop'+collisionRCollect[0].object.name);
-//             toErase.visible = false;
-//             collisionRCollect[0].object.visible = false;
-//             playAudio(pick);
-//             points += 1;
-//             if(points == 7){
-//                 document.getElementById("win").style.display = "block";
-//                 pauseAudio(song);
-//                 playAudio(wins);
-//                 gamewon=true;
-//             }
-//             i += 20;
-//         } else {
-//             document.getElementById("points").innerHTML = points;
-//         }
-//     }
-// }
-
-var i = 100;
-
-function fuelInteractions() {
-    if (i == 100 && !gamewon) {
-        i = 99;
-        var elem = document.getElementById("myBar");
-        var width = 99;
-        var id = setInterval(frame, 100);
-
-        function frame() {
-            if (width <= 0) {
-                clearInterval(id);
-                i = 0;
-                //you loose
-                document.getElementById("lost").style.display = "block";
-                if(gamewon==false){
-                    pauseAudio(song);
-                    playAudio(end);
-                }
-            } else {
-                i--;
-                width = i;
-                elem.style.width = width + "%";
-            }
-        }
+        if (collisionRCollect.length > 0 && collisionRCollect[0].distance < directionVector.length()) {
+            console.log("Collision");
+            // console.log(scene.getObjectByName(collisionRCollect[0].object.name));
+            // document.getElementById("points").innerHTML = points;
+            // var toErase = scene.getObjectByName('drop'+collisionRCollect[0].object.name);
+            // toErase.visible = false;
+            // collisionRCollect[0].object.visible = false;
+            // playAudio(pick);
+            // points += 1;
+            // if(points == 7){
+            //     document.getElementById("win").style.display = "block";
+            //     pauseAudio(song);
+            //     playAudio(wins);
+            //     gamewon=true;
+            // }
+            // i += 20;
+        } 
     }
-
 }
 
-// function autoRefreshTip() {
-//     var pos = getRandomArbitrary(0, 3);
-//     document.getElementById("tips").innerHTML = tips[pos];
 
-// }
-// setInterval(autoRefreshTip, interval); // Time is set in milliseconds
-// function getRandomArbitrary(min, max) {
-//     return Math.floor(Math.random() * (max - min)) + min;
-// }
